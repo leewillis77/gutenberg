d6e2f0007e9d21598f33cec36d0d687f31a58866
Adam Zieliński <adam@adamziel.com>
2020-11-16 15:18:00 +0100

    Try to remove effects.js middleware

--- packages/block-editor/src/store/index.js
+++ packages/block-editor/src/store/index.js
@@ -7,7 +7,6 @@ import { registerStore } from '@wordpress/data';
  * Internal dependencies
  */
 import reducer from './reducer';
-import applyMiddlewares from './middlewares';
 import * as selectors from './selectors';
 import * as actions from './actions';
 import controls from './controls';
@@ -35,6 +34,5 @@ const store = registerStore( MODULE_KEY, {
 	...storeConfig,
 	persist: [ 'preferences' ],
 } );
-applyMiddlewares( store );
 
 export default store;
