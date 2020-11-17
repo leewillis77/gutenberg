74d2ad9ec0ee9bd4e37e4cd8d0eaea58b7a19870
Copons <Copons@users.noreply.github.com>
2020-11-17 12:53:26 +0000

    Add PHP side of the default template type definitions

--- lib/templates-sync.php
+++ lib/templates-sync.php
@@ -26,7 +26,7 @@ function _gutenberg_create_auto_draft_for_template( $post_type, $slug, $theme, $
 		array(
 			'post_type'      => $post_type,
 			'post_status'    => array( 'publish', 'auto-draft' ),
-			'title'          => $slug,
+			'name'           => $slug,
 			'meta_key'       => 'theme',
 			'meta_value'     => $theme,
 			'posts_per_page' => 1,
@@ -35,15 +35,23 @@ function _gutenberg_create_auto_draft_for_template( $post_type, $slug, $theme, $
 	);
 	$post           = $template_query->have_posts() ? $template_query->next_post() : null;
 	if ( ! $post ) {
-		wp_insert_post(
-			array(
-				'post_content' => $content,
-				'post_title'   => $slug,
-				'post_status'  => 'auto-draft',
-				'post_type'    => $post_type,
-				'post_name'    => $slug,
-			)
+		$template_post = array(
+			'post_content' => $content,
+			'post_title'   => $slug,
+			'post_status'  => 'auto-draft',
+			'post_type'    => $post_type,
+			'post_name'    => $slug,
 		);
+
+		if ( 'wp_template' === $post_type ) {
+			$default_template_types = gutenberg_get_default_template_types();
+			if ( isset( $default_template_types[ $slug ] ) ) {
+				$template_post['post_title']   = $default_template_types[ $slug ]['title'];
+				$template_post['post_excerpt'] = $default_template_types[ $slug ]['description'];
+			}
+		}
+
+		wp_insert_post( $template_post );
 	} elseif ( 'auto-draft' === $post->post_status && $content !== $post->post_content ) {
 		// If the template already exists, but it was never changed by the user
 		// and the template file content changed then update the content of auto-draft.
